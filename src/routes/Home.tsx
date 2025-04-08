855cd5b5cd0318c100985d609781e1e578198716
feelsuegood <suaj2021@gmail.com>
2025-04-08 16:44:19 +1000

    #19.2 react query

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,8 @@
 import { Grid } from "@chakra-ui/react";
 import Room from "../components/Room";
 import RoomSkeleton from "../components/RoomSkeleton";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getRooms } from "../api";
 
 interface IPhoto {
   pk: string;
@@ -21,17 +22,10 @@ interface IRoom {
 }
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [rooms, setRooms] = useState<IRoom[]>([]);
-  const fetchRooms = async () => {
-    const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
-    const json = await response.json();
-    setRooms(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    fetchRooms();
-  }, []);
+  const { isLoading, data } = useQuery<IRoom[]>({
+    queryKey: ["rooms"],
+    queryFn: getRooms,
+  });
   return (
     <Grid
       mt={10}
@@ -58,7 +52,7 @@ export default function Home() {
           <RoomSkeleton />
         </>
       ) : null}
-      {rooms.map((room) => (
+      {data?.map((room) => (
         <Room
           imageUrl={room.photos[0].file}
           name={room.name}
